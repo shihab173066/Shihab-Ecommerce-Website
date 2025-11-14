1850daa6f6edeb78887d1df7059de37081a587ef
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-15 05:04:22 +0600

    More products added and UI improved

--- store/models.py
+++ store/models.py
@@ -38,26 +38,26 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 		
-# 	@property
-# 	def shipping(self):
-# 		shipping = False
-# 		orderitems = self.orderitem_set.all()
-# 		for i in orderitems:
-# 			if i.product.digital == False:
-# 				shipping = True
-# 		return shipping
+	@property
+	def shipping(self):
+		shipping = False
+		orderitems = self.orderitem_set.all()
+		for i in orderitems:
+			if i.product.digital == False:
+				shipping = True
+		return shipping
 
-# 	@property
-# 	def get_cart_total(self):
-# 		orderitems = self.orderitem_set.all()
-# 		total = sum([item.get_total for item in orderitems])
-# 		return total 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total 
 
-# 	@property
-# 	def get_cart_items(self):
-# 		orderitems = self.orderitem_set.all()
-# 		total = sum([item.quantity for item in orderitems])
-# 		return total 
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total 
 
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
