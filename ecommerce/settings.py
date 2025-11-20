92c7e670355f616ed430cde44f1302a781b1dfc0
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-21 02:11:40 +0600

    bug fix six

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,8 +145,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # WhiteNoise storage for compressed static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = '/images/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media_uploads') # Change the path
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static/images') # Add this line if 'static/images' holds media
+]
+
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
