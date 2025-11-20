aa75ee97679410733dd2a59ab3bb72e6cdb614ee
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-21 04:34:28 +0600

    FINAL RESOLUTION: Committing all source files and database to fix 500 crash

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -5,6 +5,7 @@ Django settings for ecommerce project.
 from pathlib import Path
 import os
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # -------------------------------------------------------------
