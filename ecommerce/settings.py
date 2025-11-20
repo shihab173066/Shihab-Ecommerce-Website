3b5ccf177fc5bb664baf9af858559a0cc04037c1
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-20 22:47:01 +0600

    settings fix for UI

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,12 +128,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,"static")
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
+# Your existing source static folder
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Folder where collectstatic will gather all files for production
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# WhiteNoise storage for compressed static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
