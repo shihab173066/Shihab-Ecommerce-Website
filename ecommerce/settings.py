61b3c14e26d00ead9ae926a131b9d0bd08e0ca43
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-21 22:27:32 +0600

    made changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,11 +11,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------------------
 # SECURITY
 # -------------------------------------------------------------
-SECRET_KEY = 'django-insecure-g*c52-sw60urfkt#)8szop)2tr!imfxsnh%4#4)+@!^zivcjzb'
+# SECRET_KEY = 'django-insecure-g*c52-sw60urfkt#)8szop)2tr!imfxsnh%4#4)+@!^zivcjzb'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-default-key')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
+    '*',
     'localhost',
     '127.0.0.1',
     'shihab-ecommerce-website.onrender.com',
@@ -80,11 +82,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # -------------------------------------------------------------
 # DATABASE
 # -------------------------------------------------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
 # -------------------------------------------------------------
