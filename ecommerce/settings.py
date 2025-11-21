920f0337f18f9ae93fea08ef3cc1eedd1bdaceca
shihab173066 <shihab.hossainSH@gmail.com>
2025-11-22 01:09:43 +0600

    added build script

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,7 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g*c52-sw60urfkt#)8szop)2tr!imfxsnh%4#4)+@!^zivcjzb'
 # SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-default-key')
 
-DEBUG = True
+# DEBUG = True
+# Change this line:
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
